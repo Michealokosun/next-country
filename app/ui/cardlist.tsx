6a8cdaf8e4fcf20a948370191127a178964c5135
michealsmith1224 <michealokosun6@gmail.com>
2024-05-07 00:34:08 +0100

    updated not found

--- app/ui/cardlist.tsx
+++ app/ui/cardlist.tsx
@@ -11,21 +11,24 @@ export const CardList = ({ query }: { query: string }) => {
     );
   });
   return (
-    <div className="flex relative justify-between items-center mx-auto flex-wrap gap-3 mt-16">
-      {filter.map((country) => (
-        <div
-          key={country.name}
-          className="w-full  sm:w-1/3 md:w-mdw lg:w-1/4 xl:w-1/6 mb-4 "
-        >
-          <Link href={`/${country.numericCode}`}>
-            <Cards countr={country} />
-          </Link>
-        </div>
-      ))}
+    <div>
+      <p className="text-sm text-zinc-500 mt-5">{`(${filter.length}) country available`}</p>
+      <div className="flex relative justify-between items-center mx-auto flex-wrap gap-3 mt-16">
+        {filter.map((country) => (
+          <div
+            key={country.name}
+            className="w-full  sm:w-1/3 md:w-mdw lg:w-1/4 xl:w-1/6 mb-4 "
+          >
+            <Link href={`/${country.numericCode}`}>
+              <Cards countr={country} />
+            </Link>
+          </div>
+        ))}
 
-      <p className="text-xl md:text-4xl">
-        {filter.length === 0 ? `No data found for "${query}"` : ""}
-      </p>
+        <p className="text-xl md:text-4xl block">
+          {filter.length === 0 ? `No data found for "${query}"` : ""}
+        </p>
+      </div>
     </div>
   );
 };
